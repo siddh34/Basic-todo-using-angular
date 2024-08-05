8558f8ea7f4ee81b3e3dd01cd4c8202a2504fa38
siddh34 <ssiddharthsutar@gmail.com>
2024-08-05 12:13:55 +0530

    done with task module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { CardComponent } from './shared/card/card.component';
+import { SharedModule } from './shared/shared.module';
 import { TaskComponent } from './tasks/task/task.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { FormsModule } from '@angular/forms';
@@ -14,12 +14,8 @@ import { FormsModule } from '@angular/forms';
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
